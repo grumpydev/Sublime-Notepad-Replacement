4309936c9c31ad94d15c81ff64d67711da405769
Stefan Sundin <stefansundin@users.noreply.github.com>
2014-01-11 15:10:41 -0800

    Doh, the installer check is wrong.

--- installer/ReplaceNotepad.bat
+++ installer/ReplaceNotepad.bat
@@ -1,6 +1,6 @@
 @echo off
 @echo.
-if not exist "%~dp0SublimeLauncher.exe" (
+if not exist "%~dp0sublime_text.exe" (
 	echo Warning: Can't find sublime_text.exe. You must put these files in your
 	echo Sublime Text directory before running this file.
 	echo Press enter to continue anyway...
